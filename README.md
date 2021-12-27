4d8fea0df119ae6b9673c9768c4efc9909cac3d0
whatwewant <tobewhatwewant@outlook.com>
2021-12-27 18:18:33 +0800

    :tada:

---
+++ README.md
@@ -0,0 +1,11 @@
+# Zmicro Homebrew
+
+## Installation
+
+```bash
+brew tap zcorky/zmicro
+brew install zmicro
+```
+
+## License
+MIT
---
+++ zmicro.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Zmicro < Formula
+  desc "Zero's Service Manager"
+  homepage ""
+  url "https://github.com/zcorky/zmicro/archive/refs/tags/v1.6.10.tar.gz"
+  sha256 "6075e49dd80566cc31f4032649a1fb0499d60985bf064e3072137d81e5f48692"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./install", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test zmicro`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
